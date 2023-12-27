0c8353957c9dab3912f9e79935c4f7a11d4482e7
huuphat <phatd142@gmail.com>
2023-12-27 11:40:07 +0700

    first commit

---
+++ UI/frmSplash.Designer.cs
@@ -0,0 +1,120 @@
+﻿namespace BloodBankManagementSystem.UI
+{
+    partial class frmSplash
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.lblTitle = new System.Windows.Forms.Label();
+            this.lblSubTitle = new System.Windows.Forms.Label();
+            this.panelBG = new System.Windows.Forms.Panel();
+            this.panelMovable = new System.Windows.Forms.Panel();
+            this.timerSplash = new System.Windows.Forms.Timer(this.components);
+            this.panelBG.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // lblTitle
+            // 
+            this.lblTitle.AutoSize = true;
+            this.lblTitle.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(192)))));
+            this.lblTitle.Cursor = System.Windows.Forms.Cursors.SizeAll;
+            this.lblTitle.Font = new System.Drawing.Font("Segoe UI", 27.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblTitle.ForeColor = System.Drawing.Color.Black;
+            this.lblTitle.Location = new System.Drawing.Point(290, 171);
+            this.lblTitle.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lblTitle.Name = "lblTitle";
+            this.lblTitle.Size = new System.Drawing.Size(391, 74);
+            this.lblTitle.TabIndex = 1;
+            this.lblTitle.Text = "BLOOD BANK";
+            // 
+            // lblSubTitle
+            // 
+            this.lblSubTitle.AutoSize = true;
+            this.lblSubTitle.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(192)))));
+            this.lblSubTitle.Font = new System.Drawing.Font("Segoe UI", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblSubTitle.ForeColor = System.Drawing.Color.Black;
+            this.lblSubTitle.Location = new System.Drawing.Point(316, 262);
+            this.lblSubTitle.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lblSubTitle.Name = "lblSubTitle";
+            this.lblSubTitle.Size = new System.Drawing.Size(352, 48);
+            this.lblSubTitle.TabIndex = 2;
+            this.lblSubTitle.Text = "Management System";
+            // 
+            // panelBG
+            // 
+            this.panelBG.AccessibleRole = System.Windows.Forms.AccessibleRole.None;
+            this.panelBG.BackColor = System.Drawing.Color.WhiteSmoke;
+            this.panelBG.Controls.Add(this.panelMovable);
+            this.panelBG.Location = new System.Drawing.Point(1, 391);
+            this.panelBG.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.panelBG.Name = "panelBG";
+            this.panelBG.Size = new System.Drawing.Size(996, 58);
+            this.panelBG.TabIndex = 3;
+            // 
+            // panelMovable
+            // 
+            this.panelMovable.BackColor = System.Drawing.SystemColors.Highlight;
+            this.panelMovable.Location = new System.Drawing.Point(12, 11);
+            this.panelMovable.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.panelMovable.Name = "panelMovable";
+            this.panelMovable.Size = new System.Drawing.Size(15, 38);
+            this.panelMovable.TabIndex = 0;
+            // 
+            // timerSplash
+            // 
+            this.timerSplash.Tick += new System.EventHandler(this.timerSplash_Tick);
+            // 
+            // frmSplash
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Snow;
+            this.BackgroundImage = global::BloodBank_App.Properties.Resources.bg;
+            this.ClientSize = new System.Drawing.Size(995, 538);
+            this.Controls.Add(this.panelBG);
+            this.Controls.Add(this.lblSubTitle);
+            this.Controls.Add(this.lblTitle);
+            this.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.Name = "frmSplash";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "frmSplash";
+            this.Load += new System.EventHandler(this.frmSplash_Load);
+            this.panelBG.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Label lblTitle;
+        private System.Windows.Forms.Label lblSubTitle;
+        private System.Windows.Forms.Panel panelBG;
+        private System.Windows.Forms.Panel panelMovable;
+        private System.Windows.Forms.Timer timerSplash;
+    }
+}
